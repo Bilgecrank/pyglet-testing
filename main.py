876d311bd77a16ed5566770fcc4a2eed11352c9e
Bilgecrank <theowenreport@gmail.com>
2022-11-18 09:02:52 -0800

    Expanded the welcome.

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name, extra_message):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}\n\n{extra_message}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('PyCharm', 'Do you like to party?')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
