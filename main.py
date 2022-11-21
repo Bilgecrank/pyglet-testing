67450971a20dbd82cdbb1bc186b79458a20fda63
Bilgecrank <8432868+Bilgecrank@users.noreply.github.com>
2022-11-21 14:03:31 -0800

    Cleaning up merge conflict.

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+"""
+File: main.py
+Author: Dakota Owen
+Date: 21Nov22
+Purpose: To learn pyglet, github and freshen up on python as a language.
+"""
 
 def print_hi(name, extra_message, even_more_words):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}\n\n{extra_message}\n\n{even_more_words}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}\n\n{extra_message}\n\n{even_more_words}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Buddy', 'Do you NOT like to party?', 'I do not like to party.')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
